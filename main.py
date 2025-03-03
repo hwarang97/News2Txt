486d2a76f9a227eb869f8327cb401f5bdd980674
dolggul2 <dolggul2@gmail.com>
2025-03-03 11:12:24 +0900

    feat: add parsing and save

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import requests
-
+from bs4 import BeautifulSoup
 
 url = "https://n.news.naver.com/article/newspaper/009/0005451788?date=20250301"
-respond = requests.get(url)
-print(respond.content)
+respond = requests.get(url).text
+
+soup = BeautifulSoup(respond, "html.parser")
+title = soup.find(name="h2", id="title_area").getText()
+contents = soup.find(name="article").getText().replace('\n',' ')
+
+with open("article.txt", mode="w") as file:
+    file.write(contents)
